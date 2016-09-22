a7b71ad4fc8bd88a63cf92f663b92f65ac8bc34b
Matt Godshall <lifeinhex@gmail.com>
2016-09-21 23:53:39 -0400

    New thing, plus new take on thing.

---
+++ patterns/trirec.js
@@ -0,0 +1,29 @@
+/* Triangle mountains or something.
+ *
+ * Date: 9-21-2016
+ * Author: Matt Godshall
+ */
+
+function TriRec(ctx, max_x, max_y, scheme, scale, randLength) {
+    this.ctx = ctx;
+    this.max_x = max_x;
+    this.max_y = max_y;
+    this.color = new ColorPicker(scheme);
+    this.triangle = new Triangle(this.ctx);
+    this.scale = scale;
+    this.x_offset = Math.floor(this.scale / 2);
+    this.randLength = randLength;
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(0, 0, this.max_x, this.max_y);
+}
+
+TriRec.prototype.draw = function (x) {
+    //var x = Math.floor(this.max_x / 2);
+    var scale = this.max_y; 
+    for (var y = 0; y < (20 * 800); y += 20) {
+        this.ctx.fillStyle = this.color.alternate();
+        this.triangle.up(x, y, scale);
+        scale = Math.floor(scale * 0.95);
+    }
+}
+
--- patterns/wander.js
+++ patterns/wander.js
@@ -18,7 +18,13 @@ function WanderingLines(ctx, max_x, max_y, scheme, scale, randLength) {
 
 WanderingLines.prototype.draw = function () {
     for (var x = 0; x < this.max_x; x += this.x_offset) {
-        this.ctx.strokeStyle = this.color.choose();
+        //this.ctx.strokeStyle = this.color.choose();
+        if (x >= Math.floor(this.max_x * .75) ||
+            x <= Math.floor(this.max_x * .25)) {
+            this.ctx.strokeStyle = "#ccc";
+        } else {
+            this.ctx.strokeStyle = this.color.choose();
+        }
         this._drawLine(x);
     }
 }
@@ -33,14 +39,31 @@ WanderingLines.prototype._drawLine = function (x) {
         limit = this.max_y;
     }
 
-    var noiseRange = Math.floor(this.scale * 0.1);
+    //var noiseRange = Math.floor(this.scale * 0.1);
+    var noiseRange = Math.floor(this.scale * Math.min(Math.max(Math.random(), 0.1), 0.2));
+    var noiseRangePeak = Math.floor(this.scale * Math.min(Math.max(Math.random(), 0.3), 0.8));
 
     this.ctx.beginPath();
     this.ctx.moveTo(x, 0);
-    for (var y = 0; y < limit; y += 20) {
+    for (var y = 0; y < limit + 20; y += 20) {
+        if (y >= Math.floor(limit * .75)) {
+            noise = rand0(noiseRangePeak);
+        } else {
+            noise = rand0(noiseRange);
+        }
         noise = rand0(noiseRange);
         this.ctx.lineTo(x + noise, y);
     } 
     this.ctx.stroke();
 }
 
+WanderingLines.prototype._clear = function () {
+    this.ctx.clearRect(0, 0, this.max_x, this.max_y);
+}
+
+WanderingLines.prototype.run = function () {
+    this._clear();
+    this.ctx.fillStyle = "#000";
+    this.ctx.fillRect(0, 0, this.max_x, this.max_y);
+    this.draw();
+}
